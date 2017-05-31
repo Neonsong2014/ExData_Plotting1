2de236d8322c3bd7f8ca41e99814bda9476935dd
Neonsong2014 <jian.song12@gmail.com>
2017-05-31 11:44:52 -0700

    Project 1 Assignment Submission
    Four png files and their related R script files.

---
+++ plot2.R
@@ -0,0 +1,78 @@
+# Name: plot2
+# Purpose: plot global active power against selected dates(converted to week days)
+# and save it to a png file as plot2.png
+# the given dates are "01/02/2007", "02/02/2007" 
+
+plot2 <- function()
+{
+  dates <- c("01/02/2007", "02/02/2007" ) 
+  plot_2(dates)
+}
+plot_2<- function (dates) {
+  
+  path = getwd()
+  png(filename = paste(path,"/plot2.png", sep=""), width=480, height= 480, units="px", pointsize = 12,     bg="white")
+  
+  sourcedf <- createdataframe(dates, 10000)
+  summar <- summarise(group_by(sourcedf, Date), marks=n())
+  days <- weekdays(as.Date(summar[[1]], "%d/%m/%Y"), TRUE)
+  days <- c(days, weekdays(as.Date(summar[[1]][dim(summar)[1]], "%d/%m/%Y") + 1, TRUE))
+  times <- c(1)
+  for(i in (1:dim(summar)[1]))
+  {
+    times <- c(times, times[i] + summar[[2]][i])
+  }
+  
+  plot(x=1:dim(sourcedf)[1], y= as.numeric(sourcedf$Global_active_power), type="l", ylab="Global Active Power (kilowatts)", xaxt="n", xlab="")
+  axis(1, at = times, labels = days)
+  
+  dev.off()
+}
+
+
+# Name: createdataframe
+# Purpose: Create a dataframe that contains releveant data records for given dates.
+# Because of large size of the data file it is preferred to read only concerned records 
+# from the source data file into a dataframe in memory.  This is to avoid the pitfall 
+# that computer RAM is not big enough to hold the entire data records in memory.
+# Use read.table method to read first block of rows (pre defined number such as 10000 rows) 
+# that include the first row as header for the data frame to be created.  Then subset any rows 
+# from this newly created data frame whose values in its first column Date match the given dates 
+# and assign it to itself.  This data frame will be the final output data frame that contains 
+# only date matched rows.
+# Repeat the process to continue reading a block of rows to create a working data frame within a while loop, 
+# save the working data frame's row nubmer, and then subset any rows from this working data frame 
+# whose date match the given dates and assign it to itself. 
+# Then call rbind to append current subsetted working data frame to the first data frame.
+# If saved row number of current working data frame is not equal to the pre defined block number 
+# it means it has reached the end of file. So the process stops. 
+# The fianl output is a data frame contains all rows with matching dates.
+# 
+# In case the last block of rows in the source text file matches the predefined block number an error 
+# would be raised because there are no more rows to read from the source text file.  
+# The work around this is to change the predefined block of rows to read.  
+
+# Author: Jian Song
+# Date: 05/28/2017
+createdataframe <- function(dates, readblocksize = 1000){
+  path <- getwd()
+  sdfileconn <- file(paste(path, "/household_power_consumption.txt", sep=""), "r")
+  
+  dtFormat <- "%d/%m/%Y"
+  dates <- as.Date(dates, format = dtFormat)
+  df <- read.table(sdfileconn, header = TRUE, nrows = readblocksize, sep=";", stringsAsFactors = FALSE)
+  head(df, 10)
+  df <- subset(df, as.Date(df$Date, format = dtFormat) %in% dates)
+  
+  continue <- readblocksize
+  while (continue == readblocksize)
+  {
+    dfline <- read.table(sdfileconn, header = TRUE, nrows = readblocksize, sep=";", stringsAsFactors = FALSE)
+    continue <- dim(dfline)[1]
+    names(dfline) <- names(df)
+    dfline <- subset(dfline, as.Date(dfline$Date, format = dtFormat) %in% dates)
+    df <- rbind(df, dfline)
+  }
+  close(sdfileconn)
+  df
+}
